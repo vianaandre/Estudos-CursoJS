8618b803a291f1d9fe0380f20ce40c9d76aa09a3
André Viana <andredapperviana@gmail.com>
2020-12-14 15:24:30 -0300

    css module created

---
+++ css/js/desafio.js
@@ -0,0 +1,10 @@
+// window.onhashchange = function (e) {
+//     // pegando os hash antigos e novos
+//     const oldURL = e.oldURL.split('#')[1]// pegando só os nomes
+//     const newURL = e.newURL.split('#')[1]// pegando só os nomes
+//     console.log(oldURL, newURL)// imprimindo no console para verificar 
+//     const urlOld = document.querySelector(`#menuOpcoes a[href='#${oldURL}']`) // pegando a url 
+//     const newUrl = document.querySelector(`#menuOpcoes a[href='#${newURL}']`) // pegando a url
+//     urlOld && urlOld.classList.remove('color') // removendo um classe da url antiga 
+//     newURL && newUrl.classList.add('color') // adiciona um classe a url antiga 
+// }
